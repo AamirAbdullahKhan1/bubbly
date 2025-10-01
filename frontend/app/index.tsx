b5cb20721765593816dc137c4c3d5dde5e2de692
Aamir Khan <sengoku264@gmail.com>
2025-10-01 21:16:42 +0530

    #2 Welcome and stack

---
+++ frontend/app/index.tsx
@@ -0,0 +1,32 @@
+import {StatusBar, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { colors } from '@/constants/theme'
+import Animated, { FadeInDown } from 'react-native-reanimated'
+
+const SplashScreen = () => {
+  return (
+    <View style = {styles.container}>
+      <StatusBar barStyle={'light-content'} backgroundColor={colors.neutral900}/>
+      <Animated.Image source = {require('../assets/images/splashImage.png')}
+      entering={FadeInDown.duration(700).springify()}
+      style = {styles.logo}
+      resizeMode={'contain'}
+      />
+    </View>
+  )
+}
+
+export default SplashScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.neutral900
+    },
+    logo: {
+        height: '23%',
+        aspectRatio: 1
+    }
+})
